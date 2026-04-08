65a1df30c8c5b031ac896bde4a1c1e9278ca2248
Frederick <frederickcastro456@icloud.com>
2026-04-07 22:24:21 -0500

    Major website changes

---
+++ app/components/navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="w-full flex items-center justify-between px-6 py-4 backdrop-blur-xl bg-mist-950">
+        <div className="text-pri-black font-semibold text-lg tracking-tight">
+            <img className="h-9" src="/fleurir/logo_x1.png" alt="Fleurir Logo"/>
+        </div>
+
+        <div className="pr-2 flex items-center gap-6 font-normal text-base">
+          <a className="text-white hover:text-flower-blue hover:cursor-pointer transition">
+            Home
+          </a>
+          <a className="text-white hover:text-flower-blue hover:cursor-pointer transition">
+            Dashboard
+          </a>
+          <a className="text-white hover:text-flower-blue hover:cursor-pointer transition">
+            Support
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
