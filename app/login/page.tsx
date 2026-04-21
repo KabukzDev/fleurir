f86850d2f3816a03cd6fa87b43bf0bd1e7124bc8
Frederick <51827034+KabukzDev@users.noreply.github.com>
2026-04-21 17:58:22 -0500

    t

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image"
 import Navbar from "@/app/components/navbar"
 
-export default
+export default function Login() {
+    return (
+        <div className="min-h-screen bg-mist-950"></div>
+    );
+}
